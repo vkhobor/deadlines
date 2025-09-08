79739a72c576f07b0fe460b4873058d15236caf8
h.viktor <vkhobor@gmail.com>
2025-09-08 22:34:32 +0200

    Update Deadline.jsx

--- src/components/Deadline.jsx
+++ src/components/Deadline.jsx
@@ -120,7 +120,7 @@ const Deadline = () => {
   return (
     <>
       <div
-        className={`absolute left-0 top-0 hover:border ${deadlines.length === 0 && 'border'} bg-transparent w-16 h-16 flex items-center justify-center pl-0 ml-4 mt-4  cursor-pointer`}
+        className={`absolute left-0 top-0 hover:border ${deadlines && deadlines.length === 0 && 'border'} bg-transparent w-16 h-16 flex items-center justify-center pl-0 ml-4 mt-4  cursor-pointer`}
         onClick={() => setIsModalOpen(true)}
       >
         +
